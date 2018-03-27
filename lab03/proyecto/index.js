269097a95259676e5c8a6e48bf189d1292b6c543
JOHAN\jj_al <j.j_alucard@hotmail.com>
2018-03-26 18:29:43 -0600

    First commit

---
+++ lab03/proyecto/index.js
@@ -0,0 +1,82 @@
+var param_replacer = require('./lib/replace');
+
+var objetivo = "%hello% %world%!";
+var idioma = "ch";
+var reemplazos = {
+	"en": {
+		"hello": "Hello",
+		"world": "World" 
+	},
+	"es":{
+		"hello": "Hola",
+		"world": "Mundo"
+	},
+	"fr":{
+		"hello": "Salut",
+		"World": "Mondo"
+	},
+	"it":{
+		"hello": "Ciao",
+		"world": "Mondo"
+	},
+	"ch":{
+		"hello": "La wea",
+		"world": "fome"
+	}
+};
+
+var resultado = param_replacer.replace(objetivo, reemplazos[idioma]);
+
+console.log(resultado);
+
+/*var _ = require("underscore");
+
+var employeesCollection = [
+	{
+		"id":1,
+		"name":"Soni",
+		"designation":"SE",
+		"salary":25000
+	},
+	{
+		"id":2,
+		"name":"Rohit",
+		"designation":"SSE",
+		"salary":35000
+	},
+	{
+		"id":3,
+		"name":"Akanksha",
+		"designation":"Manager",
+		"salary":45000
+	},
+	{
+		"id":4,
+		"name":"Mohan",
+		"designation":"Accountant",
+		"salary":30000
+	},
+	{
+		"id":5,
+		"name":"Gita",
+		"designation":"SSE",
+		"salary":35000
+	}
+];
+var cargos = _.map(employeesCollection,function(employee){
+	return {
+		nombre: employee.name,
+		cargo: employee.designation
+	};
+});
+
+var empleados_sse = _.chain(employeesCollection)
+	.filter(function(employee){
+		return employee.designation === 'SSE';
+	})
+	.map(function(employee){
+		return{name:employee.name,id:employee.id};
+	})
+	.value();
+
+	console.log(empleados_sse);*/
